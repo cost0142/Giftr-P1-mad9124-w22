12446bfa03451024d8e73e4572645f252bec6b7e
Hygor Costa <cost0142@algonquinlive.com>
2022-04-05 20:57:49 -0400

    add Person Schema.js

---
+++ models/Person.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 254,
+      required: true,
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    gifts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Gift" }],
+    imageUrl: { type: String, maxlength: 1024 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Model = mongoose.model("Course", schema);
+
+export default Model;

dca9c70bd72aceefa52ecf6589076ec5e4b84d55
Hygor Costa <cost0142@algonquinlive.com>
2022-04-07 17:14:25 -0400

    commented all fie for test

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,18 +1,18 @@
-import User from "../models/User.js";
+// import User from "../models/User.js";
 
-export default async function (req, res, next) {
-  const user = await User.findById(req.user._id);
-  if (user.isAdmin === true) {
-    next();
-  } else {
-    return res.status(403).send({
-      errors: [
-        {
-          status: "403",
-          title: "Access forbidden",
-          description: "Access restricted to a certain group of users",
-        },
-      ],
-    });
-  }
-}
+// export default async function (req, res, next) {
+//   const user = await User.findById(req.user._id);
+//   if (user.isAdmin === true) {
+//     next();
+//   } else {
+//     return res.status(403).send({
+//       errors: [
+//         {
+//           status: "403",
+//           title: "Access forbidden",
+//           description: "Access restricted to a certain group of users",
+//         },
+//       ],
+//     });
+//   }
+// }

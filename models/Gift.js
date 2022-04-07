b39afcac1f795eb90377527731febd6cceed3f64
Hygor Costa <cost0142@algonquinlive.com>
2022-04-07 17:50:36 -0400

    change gift schema

--- models/Gift.js
+++ models/Gift.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: { type: String, required: true, min: 4, max: 64 },
+  name: { type: String, required: true, minlength: 4, maxlength: 64 },
   price: { type: Number, min: 100, default: 1000 }, //NOT SURE HOW DEFAULT WORK
   imageUrl: { type: String, min: 1024 },
-  store: { type: Object },
-  storeName: { type: String, max: 254 },
-  storeProductURL: { type: String, max: 1024 },
+  store: {
+    name: { type: String, maxlength: 254 },
+    productURL: { type: String, maxlength: 1024 },
+  },
 });
 
 const Model = mongoose.model("Gift", schema);

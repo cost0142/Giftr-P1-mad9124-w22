16ec76385c676f2bb865c3b210928c883309e203
Hygor Costa <cost0142@algonquinlive.com>
2022-04-11 21:56:34 -0400

    add authOwner Middleware"

---
+++ middleware/authOwner.js
@@ -0,0 +1,19 @@
+import User from "../models/User.js";
+import Person from "../models/Person.js";
+
+export default async function (req, res, next) {
+  const person = await Person.findById(req.params.id);
+  if (person.owner == req.user._id) {
+    next();
+  } else {
+    return res.status(403).send({
+      errors: [
+        {
+          status: "403",
+          title: "Access forbidden",
+          description: "Access restricted to a certain group of person.",
+        },
+      ],
+    });
+  }
+}

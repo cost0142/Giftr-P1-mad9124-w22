f44842a18f1ac8664d9b0628d742d9d6fbbecc64
sun00120 <sun00120@algonquinlive.com>
2022-04-12 20:14:13 -0400

    add logger file to connectDatabase and server JS.

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import http from "http";
 import app from "./app.js";
-import createDebug from "debug";
-
-const debug = createDebug("week9:httpServer");
+import log from "./logger.js";
 
 const httpServer = http.createServer(app);
 
 const port = process.env.PORT || 3030;
 httpServer.listen(port, () => {
-  debug(`HTTP server listening on port ${port}`);
+  log.info(`HTTP server listening on port ${port}`);
 });

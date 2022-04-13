785b7cf93a32db16a652da8f3a3e96ef4bdc09d8
Hygor Costa <cost0142@algonquinlive.com>
2022-04-12 21:34:34 -0400

    Change connect Data Base js

--- startup/connectDatabase.js
+++ startup/connectDatabase.js
@@ -1,18 +1,33 @@
+import config from "config";
 import mongoose from "mongoose";
 import logger from "./logger.js";
 
 const log = logger.child({ module: "connectDB" });
 
-export default function () {
-  mongoose
-    .connect("mongodb://localhost:27017/mad9124", {
+export default async function connectDatabase() {
+  const { scheme, host, port, name, username, password, authSource } =
+    config.get("db");
+  const credentials = username && password ? `${username}:${password}@` : "";
+
+  let connectionString = `${scheme}://${credentials}${host}`;
+
+  if (scheme === "mongodb") {
+    connectionString += `:${port}/${name}?authSource=${authSource}`;
+  } else {
+    connectionString += `/${authSource}?retryWrites=true&w=majority`;
+  }
+
+  try {
+    await mongoose.connect(connectionString, {
       useNewUrlParser: true,
-    })
-    .then(() => {
-      log.info("Successfully connected to MongoDB ...");
-    })
-    .catch((err) => {
-      log.error("Error connecting to MongoDB ... ", err.message);
-      process.exit(1);
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      dbName: name,
     });
+    log.info(`Connected to MongoDB @ ${name}...`);
+  } catch (err) {
+    log.error(`Error connecting to MongoDB ...`, err);
+    process.exit(1);
+  }
 }

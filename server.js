a324a6835c10a8f20b0df17b704ecbf27c24f72d
sun00120 <sun00120@algonquinlive.com>
2022-04-12 20:26:48 -0400

    fix logger

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from "http";
 import app from "./app.js";
-import log from "./logger.js";
+import createDebug from "debug";
+
+const debug = createDebug("week9:httpServer");
 
 const httpServer = http.createServer(app);
 
 const port = process.env.PORT || 3030;
 httpServer.listen(port, () => {
-  log.info(`HTTP server listening on port ${port}`);
+  debug(`HTTP server listening on port ${port}`);
 });
--- startup/connectDatabase.js
+++ startup/connectDatabase.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import log from "./logger.js";
+import logger from "./logger.js";
 
 const log = logger.child({ module: "connectDB" });
 
@@ -7,9 +7,6 @@ export default function () {
   mongoose
     .connect("mongodb://localhost:27017/mad9124", {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
     })
     .then(() => {
       log.info("Successfully connected to MongoDB ...");
